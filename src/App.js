b2dfe0574c2bf0c0cb6b607d5dc3c9d340f1c195
Alex Mendes <mendes.develop@gmail.com>
2020-02-10 09:21:02 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import './App.css';
+
+
+class App extends React.Component {
+
+  render(){
+    return (
+      <div className="App">
+       {1 + 1}
+      </div>
+    );
+  }
+}
+
+export default App;
